fbb3ca0c9ee17e9bbb10e49dda5cfc6d4b41acda
Anthony Smiderle <anth19800@hotmail.com>
2022-01-03 03:13:48 -0500

    [Add] Movement, asteroids

---
+++ Assets/Scripts/AsteroidScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidScript : MonoBehaviour
+{
+    [SerializeField] float maxSpawnSpeed = 1.0f;
+    [Header("References")]
+    [SerializeField] GameObject asteroidPrefab;
+    [HideInInspector] public int size = 3;
+
+    float _zRot = 0.0f;
+
+    private void Start()
+    {
+        _zRot = Random.Range(-1.0f, 1.0f);
+    }
+
+    private void Update()
+    {
+        transform.Rotate(-Vector3.forward * _zRot);
+    }
+
+    public void OnDie()
+    {
+        var asOne = GameObject.Instantiate(asteroidPrefab);
+        var asTwo = GameObject.Instantiate(asteroidPrefab);
+        asOne.GetComponent<AsteroidScript>().size = size - 1;
+        asTwo.GetComponent<AsteroidScript>().size = size - 1;
+        
+        asOne.transform.localScale = asOne.transform.localScale / 2.0f;
+        asTwo.transform.localScale = asTwo.transform.localScale / 2.0f;
+        
+        asOne.transform.position = transform.position;
+        asTwo.transform.position = transform.position;
+
+        asOne.transform.Rotate(-Vector3.forward,Random.Range(0.0f,360.0f));
+        asOne.GetComponent<Rigidbody2D>().AddForce(asOne.transform.up*maxSpawnSpeed);
+
+        asTwo.transform.Rotate(-Vector3.forward,Random.Range(0.0f,360.0f));
+        asTwo.GetComponent<Rigidbody2D>().AddForce(asTwo.transform.up*maxSpawnSpeed);
+    }
+}
---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+
+public class ShipController : MonoBehaviour
+{
+
+    public float moveSpeed = 1.0f;
+    public float maxSpeed = 10.0f;
+    public float rotationSpeed = 1.0f;
+
+    [Header("References")]
+    [SerializeField] new Rigidbody2D _rigidbody;
+
+    Vector2 _moveVec = new Vector2();
+
+    private void Update()
+    {
+
+
+    }
+    private void LateUpdate()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+        if (_moveVec.x != 0.0f)
+            _rigidbody.MoveRotation(_rigidbody.rotation - _moveVec.x*rotationSpeed);
+
+        var dir = moveSpeed * -_rigidbody.gameObject.transform.up;
+        if (_moveVec.y > 0.0f)
+            _rigidbody.AddForce(dir);
+        else if (_moveVec.y < 0.0f)
+            _rigidbody.AddForce(-_rigidbody.velocity);
+
+        if (_rigidbody.velocity.magnitude > maxSpeed)
+        {
+            _rigidbody.AddForce(-_rigidbody.velocity);
+        }
+    }
+
+    public void OnMove(CallbackContext ctx)
+    {
+        _moveVec = ctx.ReadValue<Vector2>();
+    }
+    public void OnPause(CallbackContext ctx)
+    {
+
+    }
+}
